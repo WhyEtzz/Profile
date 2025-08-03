75229e86b1ead56e573b3e666357747ad12d84ec
Whyetzz <75259101+WhyEtzz@users.noreply.github.com>
2025-08-03 15:59:45 +0700

    Update credits.js

--- src/app/credits.js
+++ src/app/credits.js
@@ -7,6 +7,7 @@ const myFont2 = localFont({
 })
 export default function Credits() {
     return (
-        <div className={"flex flex-row lg:text-start text-center px-2 lg:px-20 py-5 bg-black lg:justify-start justify-center "+myFont2.className}><Link className="text-white cursor-none" href={'#'} scroll={true}>Code With ♥️ From Mochammad Ekhi Setiawan</Link></div>
+        <div className={"flex flex-row lg:text-start text-center px-2 lg:px-20 py-5 bg-black lg:justify-start justify-center "+myFont2.className}><Link className="text-white cursor-none" href={'#'} scroll={true}>Code With ♥️ By Mochammad Ekhi Setiawan</Link></div>
     )
+
 }
